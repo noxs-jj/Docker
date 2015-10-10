85005effe711c9a23a227eeaa6757b271b46210f
Jean-jacques MOIROUX <jmoiroux@e1r2p12.42.fr>
2015-10-10 16:42:37 +0200

    update torrents

--- torrent_debian_transmission/3_share_folder_macosx.sh
+++ torrent_debian_transmission/3_share_folder_macosx.sh
@@ -1,11 +1,1 @@
-## Base image Ubuntu 14.04
-FROM ubuntu:14.04
 
-MAINTAINER Jean-Jacques MOIROUX <jjmoiroux@gmail.com>
-
-## Set local for postgresql to suppress bug
-RUN locale-gen en_US.UTF-8 && echo 'LANG="en_US.UTF-8"' > /etc/default/locale
-
-## install following packets with auto accept
-RUN apt-get update ; apt-get install -y curl  nano  openssh-server wget transmission-daemon \
-		&& apt-get clean ; rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
